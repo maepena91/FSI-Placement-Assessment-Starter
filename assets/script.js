e8e09ef7a7fa0ef545b070ad4709bc417f2b3d06
Mae Pena <maepena@Maes-MacBook-Pro.local>
2021-11-22 21:24:07 -0800

    submission

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,38 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Mae Pena" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let gb = 0;      // Gingerbread
+let cc = 0;      // Chocolate Chip
+let sugar = 0;   // Sugar Sprinkle
 
-// Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+//selectors for each individual cookie 
+let Gb0 = document.querySelector("#add-gb");
+let CC0 = document.querySelector("#add-cc");
+let sugar0 = document.querySelector("#add-sugar");
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+
+//create and append gingerbread quantity button
+document.createElementById("#add-gb").addEventListener('click', function(){
+    console.log('Ginger bread + button is clicked')
+    Gingerbread = Gingerbread + 1;
+    document.querySelector("#add-gb").textContent = Gingerbread;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+
+//create and append chocolate chip quantity button
+document.createElementById("#add-cc").addEventListener('click', function(){
+let ChocolateChip = ChocolateChip + 1;
+document.querySelector("#add-cc").textContent = ChocolateChip
+
+})
+
+//create and append sugar sprinkle cookie
+let Sugar = document.createElementById("#add-sugar").addEventListener('click', function(){
+    Sugar = Sugar + 1;
+document.querySelector("#add-sugar").textContent = Sugar
+
+})
